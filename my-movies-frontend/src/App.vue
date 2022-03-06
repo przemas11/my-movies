63ae569015069899acfddf07a43e24ab0b1693e6
przemas11 <przemyslaw.malinski2@wp.pl>
2022-03-06 01:50:22 +0100

    Added MovieList and MountingPortal for modal in App

--- my-movies-frontend/src/App.vue
+++ my-movies-frontend/src/App.vue
@@ -1,26 +1,30 @@
 <script setup>
-    import HelloWorld from './components/HelloWorld.vue';
-    export default {
-        components: {
-            HelloWorld,
-        },
-    };
+import MovieList from "./components/MovieList.vue";
+
+export default {
+  components: {
+    MovieList,
+  },
+};
 </script>
 
 <template>
-    <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png" />
-        <HelloWorld msg="Hello Vue 2 + Vite" />
-    </div>
+  <div id="app">
+    <MovieList />
+
+    <MountingPortal mountTo="#modal-target">
+      <PortalTarget name="modal-target" />
+    </MountingPortal>
+  </div>
 </template>
 
 <style>
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
