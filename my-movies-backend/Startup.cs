7839400e6b6bc5eb8c153fc586d03bc923af6ada
przemas11 <przemyslaw.malinski2@wp.pl>
2022-03-06 19:46:49 +0100

    Added configuration to use MySQL database

--- my-movies-backend/Startup.cs
+++ my-movies-backend/Startup.cs
@@ -26,7 +26,11 @@ namespace my_movies_backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // use SQL Server
             services.AddDbContext<MoviesContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            // use MySQL
+            //services.AddDbContext<MoviesContext>(options => options.UseMySql(Configuration.GetConnectionString("MySQLConnection")));
 
             services.AddControllers();
         }
