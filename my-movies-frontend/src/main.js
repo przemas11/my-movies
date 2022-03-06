9060e4ce77e8a079a173886bb83c20b7dedc55c4
przemas11 <przemyslaw.malinski2@wp.pl>
2022-03-06 01:47:52 +0100

    Added PortalVue and Vuelidate

--- my-movies-frontend/src/main.js
+++ my-movies-frontend/src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
+import PortalVue from "portal-vue";
+import Vuelidate from 'vuelidate';
+Vue.use(PortalVue);
+Vue.use(Vuelidate);
 
 new Vue({
     render: (h) => h(App),
